738d63006b331ecc05f2356e316912c3a6ce7b39
Rahini_Sathish <141403412+RahiniSathish@users.noreply.github.com>
2025-03-27 19:02:29 +0530

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 import models, schemas
 
 def create_document(db: Session, document: schemas.DocumentCreate):
-    db_document = models.Document(**document.model_dump())  # model_dump() instead of dict()
+    db_document = models.Document(**document.model_dump())  
     db.add(db_document)
     db.commit()
     db.refresh(db_document)
