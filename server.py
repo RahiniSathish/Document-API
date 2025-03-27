924b3101189b19b6cfb77a4505a3dc04e0287ea1
Rahini_Sathish <141403412+RahiniSathish@users.noreply.github.com>
2025-03-27 19:05:42 +0530

    Update server file

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import uvicorn
-from app.main import app  # ensure correct relative import
-
+from app.main import app 
 if __name__ == "__main__":
-    print("🚀 Starting FastAPI Server from server.py...")
+    print("Starting FastAPI Server from server.py...")
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
